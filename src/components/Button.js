0a53dd167f2f34628b2f86f620e53fa79c645285
Guilhermano Silva <guilhermanodev@gmail.com>
2023-09-07 10:46:54 -0300

    feat: create basic components

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  width: 100%;
+  height: 52px;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  box-shadow: 0px 4px 10px rgba(0,0,0,0.4);
+  background-color: ${({ theme }) => theme.colors.primary.main};
+  color: #fff;
+  transition: background 0.2s ease-in;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primary.light};
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.primary.dark};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.gray[200]};
+    cursor: default;
+  }
+`;
---
+++ src/components/Select.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  padding: 0 16px;
+  height: 52px;
+  background-color: #fff;
+  box-shadow: 0px 4px 10px rgba(0,0,0,0.4);
+  border-radius: 4px;
+  font-size: 16px;
+  border: 2px solid #fff;
+  outline: none;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+  }
+`;
