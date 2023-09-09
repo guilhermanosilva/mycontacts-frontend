85e17dce56d685096812b286d4e9bdb5f9fecc7c
Guilhermano Silva <guilhermanodev@gmail.com>
2023-09-09 19:21:07 -0300

    feat: handle form input error

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,6 @@ import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
-import Loader from '../../components/Loader';
 import {
   Card,
   Container,
@@ -16,8 +15,6 @@ import {
 export default function Home() {
   return (
     <Container>
-      <Loader />
-
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar pelo none..." />
       </InputSearchContainer>
