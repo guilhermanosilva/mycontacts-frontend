68c6aa4e1113f3a41ff8662809fa6ab181cd6f18
Guilhermano Silva <guilhermanodev@gmail.com>
2023-10-02 20:40:35 -0300

    refactor: add useCallback in useErros hook

--- src/hooks/useErrors.js
+++ src/hooks/useErrors.js
@@ -1,23 +1,23 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 export default function useErrors() {
   const [errors, setErrors] = useState([]);
 
-  function setError({ field, message }) {
+  const setError = useCallback(({ field, message }) => {
     const emailAlreadyExists = errors.find((error) => error.field === field);
 
     if (emailAlreadyExists) return;
 
     setErrors((prev) => [...prev, { field, message }]);
-  }
+  }, [errors]);
 
-  function removeError(fieldName) {
+  const removeError = useCallback((fieldName) => {
     setErrors((prev) => prev.filter((error) => error.field !== fieldName));
-  }
+  }, []);
 
-  function getErrorMessageByFieldName(fieldName) {
-    return errors.find((error) => error.field === fieldName)?.message;
-  }
+  const getErrorMessageByFieldName = useCallback((fieldName) => (
+    errors.find((error) => error.field === fieldName)?.message
+  ), [errors]);
 
   return {
     errors, setError, removeError, getErrorMessageByFieldName,
